a45f9359386054a20491141a10f59a8d3f132bd8
Meezo06 <moazzibrahim510@gmail.com>
2025-12-10 15:14:29 +0200

    Complete URL handling

--- popup.js
+++ popup.js
@@ -6,26 +6,38 @@ async function load() {
                 currentWindow: true
             }
         )
-        console.log(currentTabUrl);
         return currentTabUrl;
     }
 
     const isGithubRepo = async (url) => {
-        const repoRegex = /^https:\/\/github\.com\/([^\/]+)\/([^\/]+)\/?$/;
-        const {ok} = await fetch(url);
-        return repoRegex.test(url) && ok;
+        const {ok: isOk} = await fetch(url);
+        return isOk;
     }
 
     const noteFormBtn = document.getElementById("note-form-btn");
-    const newNoteForm = document.querySelector("form");
+    const newNoteForm = document.querySelector(".new-note");
     const cancelNoteBtn = document.getElementById("cancel-note-btn");
     const urlInput = document.getElementById("url-input");
+    const addNoteBtn = document.getElementById("add-note-btn");
+    const noteInput = document.getElementById("note-input")
     noteFormBtn.onclick = () => newNoteForm.style.display = "block";
     cancelNoteBtn.onclick = () => newNoteForm.style.display = "none";
 
 
     let currentUrl = await getCurrentTabUrl();
     await isGithubRepo(currentUrl) ? urlInput.value = currentUrl : urlInput.value = "";
+
+    addNoteBtn.addEventListener('click', () => {
+        // change input handling later
+        if (!urlInput.value) alert("Please Enter a URL"); 
+        else if (!noteInput.value) alert("Please Enter a note to add");
+        else {
+            const repoRegex = /^(?:https?:\/\/[^/]+)?(\/?[^/?#]+(?:\/[^/?#]+)?)/;
+            const [, repo] = urlInput.value.match(repoRegex);
+            if(!repo.startsWith("/")) repo = "/" + repo;
+        }
+
+    })
 }
 
 load();
