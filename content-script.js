555edcdfe03f977d2b0688338bdac3e666c15f48
Meezo06 <moazzibrahim510@gmail.com>
2025-12-23 18:00:03 +0200

    Setting up line notes

--- content-script.js
+++ content-script.js
@@ -1,14 +1,14 @@
 if (document.getElementById("highlighted-line-menu-positioner")) {
     const lines = document.querySelectorAll(".react-line-number");
-    lines.forEach((line, index) => {
+    lines.forEach((line) => {
+        const modal = document.createElement("dialog");
         const btn = document.createElement("button");
-        btn.style.cursor = "pointer";
-        btn.style.padding = "0";
-        btn.style.border = "none";
-        btn.style.backgroundColor = "transparent";
+        btn.textContent = "📝";
+        btn.className = "line-note-btn";
         btn.onclick = () => {
-            
+            modal.showModal();
         }
+        line.insertAdjacentElement("afterbegin", modal);
         line.insertAdjacentElement('afterbegin', btn);
     });
 }
