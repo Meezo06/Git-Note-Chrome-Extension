14978d63924605c226c88bb14df5d772051d7ffb
Meezo06 <moazzibrahim510@gmail.com>
2025-12-31 20:09:13 +0200

    Untested Line Notes logic

--- content-script.js
+++ content-script.js
@@ -1,91 +1,113 @@
-const lineMenu = document.getElementById("highlighted-line-menu-positioner");
+const lineMenu = document.getElementById("highlighted-line-menu-positioner");    
+const lines = document.querySelectorAll(".react-line-number");
 const fileRegex = /\/[^/]+\/[^/]+\/blob\/[^/]+(\/.+)/;
 const repoRegex = /^(\/[^/]+\/[^/]+)/;
 const [, file] = location.pathname.match(fileRegex);
 const repo = location.pathname.match(repoRegex)[1].replace(/#.*/, "");
+const commitHash = document.querySelector('.flex-nowrap .Link--secondary').textContent;
 chrome.storage.local.remove(repo);
-(async () => {
-    if (lineMenu) {
-        await showLineNotes();
-        const modal = document.createElement("dialog");
-        modal.id = "lines-modal";
-        modal.innerHTML = `
-            <div class="modal-header">
-                <span>Add line note</span>
-                <button class="close-icon" aria-label="Close">
-                    <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="16" height="16" fill="currentColor">
-                        <path d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L9.06 8l3.22 3.22a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L8 9.06l-3.22 3.22a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z"></path>
-                    </svg>
-                </button>
-            </div>
-            <div class="modal-body">
-                <textarea placeholder="Leave a comment"></textarea>
-                <div class="modal-footer">
-                    <button class="primary-btn">Add Note</button>
-                </div>
-            </div>
-        `;
-        lineMenu.insertAdjacentElement("afterbegin", modal);
-        document.querySelector(`#${modal.id} .close-icon`).onclick = () => modal.close();
-        const addNoteBtn = document.querySelector(`#${modal.id} .primary-btn`);
-        const noteInput = document.querySelector(`#${modal.id} textarea`);
-        const lines = document.querySelectorAll(".react-line-number");
-        lines.forEach((line, index) => {
-            const btn = document.createElement("button");        
-            btn.textContent = "📝";
-            btn.className = "line-note-btn";
-            btn.onclick = () => {
-                modal.showModal();
-                addNoteBtn.onclick = async () => {
-                    if (!noteInput.value) {
-                        alert("Plase enter some note") // change handling later
-                        return;
-                    }
-                    const repoObj = await chrome.storage.local.get(repo);
-                    const noteObj = {
-                        line: index + 1,
-                        note: noteInput.value,
-                        createdAt: new Date()
-                    }
-                    if (repo in repoObj) {
-                        if (!(file in repoObj[repo].files)) {
-                            repoObj[repo].files[file] = [];
-                        }
-                        repoObj[repo].files[file].push(noteObj);
-                        chrome.storage.local.set(repoObj);
-                    }
-                    else {
-                        const newRepo = {
-                            [repo]: {
-                                generalNotes: [],
-                                files: {
-                                    [file]: []
-                                }
-                            } 
-                        }
-                        newRepo[repo].files[file].push(noteObj);
-                        chrome.storage.local.set(newRepo);
-                    }
-                    console.log(noteObj);
-                    modal.close();
-                    await showLineNotes();
-                }
-            }
-            line.insertAdjacentElement('afterbegin', btn);
-        });
-    }
-})()
 
+if (lineMenu) {
+    showLineNotes();
+    const modal = document.createElement("dialog");
+    modal.id = "lines-modal";
+    modal.innerHTML = `
+        <div class="modal-header">
+            <span>Add line note</span>
+            <button class="close-icon" aria-label="Close">
+                <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="16" height="16" fill="currentColor">
+                    <path d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L9.06 8l3.22 3.22a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L8 9.06l-3.22 3.22a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z"></path>
+                </svg>
+            </button>
+        </div>
+        <div class="modal-body">
+            <textarea placeholder="Leave a comment"></textarea>
+            <div class="modal-footer">
+                <button class="primary-btn">Add Note</button>
+            </div>
+        </div>
+    `;
+    lineMenu.insertAdjacentElement("afterbegin", modal);
+    document.querySelector(`#${modal.id} .close-icon`).onclick = () => modal.close();
+    const addNoteBtn = document.querySelector(`#${modal.id} .primary-btn`);
+    const noteInput = document.querySelector(`#${modal.id} textarea`);
+
+    lines.forEach((line, index) => {
+        const btn = document.createElement("button");        
+        btn.textContent = "📝";
+        btn.className = "line-note-btn";
+        btn.onclick = () => {
+            modal.showModal();
+            addNoteBtn.onclick = async () => {
+                if (!noteInput.value) {
+                    alert("Plase enter some note") // change handling later
+                    return;
+                }
+                const repoObj = await chrome.storage.local.get(repo);
+                const noteObj = {
+                    line: index + 1,
+                    note: noteInput.value,
+                    createdAt: new Date(),
+                    commit: commitHash
+                }
+                if (repo in repoObj) {
+                    if (!(file in repoObj[repo].files)) {
+                        repoObj[repo].files[file] = [];
+                    }
+                    repoObj[repo].files[file].push(noteObj);
+                    chrome.storage.local.set(repoObj);
+                }
+                else {
+                    const newRepo = {
+                        [repo]: {
+                            generalNotes: [],
+                            files: {
+                                [file]: []
+                            }
+                        } 
+                    }
+                    newRepo[repo].files[file].push(noteObj);
+                    chrome.storage.local.set(newRepo);
+                }
+                modal.close();
+                showLineNotes();
+            }
+        }
+        line.insertAdjacentElement('afterbegin', btn);
+    });
+}
 
 async function showLineNotes() {
     try {
         const {[repo]: {files: {[file]: lineNotes}}} = await chrome.storage.local.get(repo);
-        lineNotes.forEach(({note, line, createdAt}) => {
+        lineNotes.forEach(({note, line, createdAt, commit}) => {
             const noteBtn = document.createElement("button");
             noteBtn.className = "note-btn";
-            noteBtn.textContent = "Note";
-            document.getElementById(`LC${line}`).insertAdjacentElement("beforeend", noteBtn);
-            console.log(note);
+            const noteContainer = document.createElement("div");
+            const noteText = document.createElement("h3");
+            const closeBtn = document.createElement("button");
+            closeBtn.innerHTML = `
+                <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="16" height="16" fill="currentColor">
+                    <path d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L9.06 8l3.22 3.22a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L8 9.06l-3.22 3.22a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z"></path>
+                </svg>
+            `
+            noteText.textContent = note + "\ncreated at: " + createdAt;
+            closeBtn.onclick = () => noteContainer.style.display = "none";
+            noteContainer.id = "note-container";
+            noteContainer.insertAdjacentElement("beforebegin", closeBtn);
+            noteContainer.insertAdjacentElement("beforeend", noteText);
+            lines[line - 1].insertAdjacentElement("beforeend", noteContainer);
+            noteBtn.onclick = () => noteContainer.style.display = "block";
+            if (commit != commitHash) {
+                noteBtn.textContent = "📕";
+                const warning = document.createElement("h4");
+                warning.textContent = "Warning: this note has been created in an older commit\n it might not refrence the line earlier"
+                noteContainer.insertAdjacentElement("beforeend", warning);
+            }
+            else {
+                noteBtn.textContent = "📗";
+            }
+            lines[line - 1].insertAdjacentElement("beforebegin", noteBtn);
         })
     }
     catch (e) {
